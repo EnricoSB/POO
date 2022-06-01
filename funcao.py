e6fe77ca1becb3e283b327e666d11363125ab31a
Enrico Barduco <98822442+EnricoSB@users.noreply.github.com>
2022-06-01 02:53:16 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,53 @@
+# def saudacao(msg='Ola', nome='enrico'):
+#     nome= nome.replace('e','3')
+#     msg= msg.replace('a', '4')
+#     return f'{msg} {nome}'
+#
+#
+# variavel = saudacao()
+# print(variavel)
+
+
+
+# '''Exercicio 1'''
+# def saudacao(msg='Olá', nome='Enrico'):
+#
+#     print(msg, nome)
+#
+# saudacao()
+# '''Exercicio 2'''
+# def soma(valor1,valor2,valor3):
+#     return valor1+valor2+valor3
+#
+# valor1=int(input('Digite um valor: '))
+# valor2=int(input('Digite um valor: '))
+# valor3=int(input('Digite um valor: '))
+#
+# resultado = soma(valor1,valor2,valor3)
+# print(resultado)
+
+# '''Exercicio 3'''
+# def porcentual(valor,percentual):
+#     return valor+((valor/100)*percentual)
+#
+# valor=int(input('Digite um valor: '))
+# percentual=int(input("Digite o percentual desejado"))
+#
+# resultado = porcentual(valor, percentual)
+# print(resultado)
+# '''Exercicio 4'''
+# def buzz(valor):
+#     if valor%3==0 and valor%5==0:
+#         return'FizzBuzz'
+#     elif  valor%3==0:
+#         return'Fizz'
+#     elif valor%5==0:
+#         return'Buzz'
+#     else: return valor
+#
+#
+# valor=int(input('Digite um numero:'))
+# resultado=''
+# resultado=buzz(valor)
+# print(resultado)
+
